8f77452e868fdb81fcb52c30d8fb0ddba7e27915
Wilfried Gruber <wilfried.gruber@htl-salzburg.ac.at>
2020-01-07 14:27:10 +0100

    Add CountUp button functionality

---
+++ Assets/Counter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public Text counterOutput;
+    public int counter = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counterOutput.text = counter.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CountUp()
+    {
+        counter += 1;
+        counterOutput.text = counter.ToString();
+    }
+}
